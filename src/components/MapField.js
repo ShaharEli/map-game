697b29a3876c8cf9a23d51d2a3a0b498be4d0bc7
ShaharEli <shahar.e3@gmail.com>
2020-10-15 12:36:51 +0300

    getting marker location

---
+++ src/components/MapField.js
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from "react";
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
+import locations from "./Data/yeshuvim.json";
+
+const mapStyles = {
+  width: "100%",
+  height: "100%",
+};
+
+const getRandomLocation = () => {
+  return locations[Math.floor(Math.random() * locations.length)];
+};
+const mapStyle = [
+  {
+    featureType: "all",
+    elementType: "labels",
+    stylers: [{ visibility: "off" }],
+  },
+];
+
+const _mapLoaded = (mapProps, map) => {
+  map.setOptions({
+    styles: mapStyle,
+  });
+};
+
+function MapField(props) {
+  const [showingInfoWindow, setShowingInfoWindow] = useState(true); // Hides or shows the InfoWindow
+  const [activeMarker, setActiveMarker] = useState({}); // Shows the active marker upon click
+  const [selectedPlace, setSelectedPlace] = useState({}); // Shows the InfoWindow to the selected place upon a marker
+  const [randomLocation, setRandomLocation] = useState({});
+  const [chosenLocation, setChosenLocation] = useState({
+    lat: 31.46667,
+    lng: 34.783333,
+  });
+
+  useEffect(() => {
+    setRandomLocation(getRandomLocation());
+  }, []);
+
+  const onMarkerClick = (props, marker, e) => {
+    setShowingInfoWindow(true);
+    setActiveMarker(marker);
+    setSelectedPlace(props);
+  };
+
+  const onClose = (props) => {
+    if (showingInfoWindow) {
+      setShowingInfoWindow(false);
+      setActiveMarker(null);
+    }
+  };
+
+  return (
+    <div>
+      <Map
+        onClick={(e, b, c) => {
+          setChosenLocation({
+            lat: c.latLng.lat(),
+            lng: c.latLng.lng(),
+          });
+        }}
+        google={props.google}
+        zoom={7.4}
+        style={mapStyles}
+        initialCenter={{
+          lat: 31.46667,
+          lng: 34.783333,
+        }}
+        onReady={(mapProps, map) => _mapLoaded(mapProps, map)}
+      >
+        <Marker
+          position={chosenLocation}
+          onClick={onMarkerClick}
+          name={"Kenyatta International Convention Centre"}
+        />
+        <InfoWindow
+          marker={activeMarker}
+          visible={showingInfoWindow}
+          onClose={onClose}
+        >
+          <div>
+            <h4>{selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </Map>
+    </div>
+  );
+}
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyA1jO5KCUbo5ifKHb4LK5ilBN2Fp0NZb5Y",
+})(MapField);
