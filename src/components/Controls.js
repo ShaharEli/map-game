1b232abe862036fd1993f5ceb3aec3521d8d2427
ShaharEli <shahar.e3@gmail.com>
2020-10-16 22:46:26 +0300

    hint counter added

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
+import Swal from "sweetalert2";
 import "./Controls.css";
 function Controls({
   reset,
@@ -8,21 +9,40 @@ function Controls({
   hintSetter,
   highScore,
 }) {
+  const [numOfHints, setNumOfHints] = useState(1);
+  const handleHints = useCallback(() => {
+    if (numOfHints > 0) {
+      setNumOfHints((prev) => prev - 1);
+      hintSetter((prev) => !prev);
+    } else {
+      Swal.fire("טעות", "נגמרו הרמזים", "error");
+    }
+  }, [numOfHints]);
+
+  const handleReset = useCallback(
+    (e) => {
+      if (roundCounter === 4) {
+        setNumOfHints(1);
+        reset(e, true);
+      } else {
+        reset(e, false);
+      }
+    },
+    [roundCounter, reset]
+  );
   return (
     <div className='controls'>
       <h1>מרחק ממטרות: {score} קילומטרים</h1>
-      <h2>המטרה: {location}</h2>
+      <div>
+        <h2>המטרה: {location}</h2>
+      </div>
       <h2>תור מספר: {roundCounter + 1}/5</h2>
       {highScore && <h2>תוצאה מירבית: {highScore}</h2>}
-      <button className='hint-btn' onClick={() => hintSetter((prev) => !prev)}>
+      <h2>מספר רמזים שנותרו: {numOfHints}</h2>
+      <button className='hint-btn' onClick={handleHints}>
         רמז
       </button>
-      <button
-        className='next-turn-btn'
-        onClick={
-          roundCounter === 4 ? (e) => reset(e, true) : (e) => reset(e, false)
-        }
-      >
+      <button className='next-turn-btn' onClick={handleReset}>
         תור הבא
       </button>
     </div>
